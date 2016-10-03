dd8388e15e8498cfb44a6c1eb881c396f9e97b6f
DGOVIL2018 <DGOVIL2018@pingry.org>
2016-10-02 20:53:57 -0400

    Add files via upload

---
+++ MontyHallGame.java
@@ -0,0 +1,110 @@
+public static MontyHallGame 
+{
+/* Created By: Dhruv Govil
+   Version#: 1
+   Play the classic Monty Hall Game-show game
+   Program is well abstracted -- each method performs an independant task to create a functional game
+
+
+**/
+	public static void main (String[] args) 
+	{	
+		//welcome message
+		welcome();
+		game();
+		
+		
+		
+	}
+
+
+	public static void welcome() //displays welcome message with instructions for the user
+	{
+		System.out.println("Monty Hall Game.");
+		System.out.print("Pick a door number first.");
+		System.out.println("You will have a chance to switch, after a door has been revealed.");
+	
+	}
+	
+	
+	public static void game() //generates random number for car, and asks user for door input and if they want to switch doors
+	{
+		Scanner keyboard = new Scanner(System.in);
+		int car = (int) (Math.random() * 3 + 1);
+		
+		System.out.print("Please choose a door (1, 2, or 3): ");
+		int user_pick = keyboard.nextInt();
+		
+		int reveal = doorToReveal(car, user_pick); //pass parameters car and user_pick to the door to reveal function
+		
+		//testing
+		System.out.println("You chose door #: " + user_pick);
+		System.out.println("Open door #: " + reveal);
+		System.out.print("Do you want to switch?: ");
+		String answer = keyboard.nextLine();
+		
+		System.out.println("The user wants to switch " + answer);
+		if(answer.equals("yes"))
+		{
+			user_pick = doorToReveal(user_pick, reveal);
+		}
+		if ( car == user_pick)  //prints game results
+			System.out.println(YOU WIN!);
+		else
+			System.out.println("Sorry! You Lose)");
+	}
+	
+	public static int doorToReveal(int c, int u) //returns which door to reveal by using logic operators
+	{
+		for (int i; i < 4; i++)
+		{
+			if (i != c && i != u)
+				return i;
+		}
+			
+		
+		
+		return -1;
+	}
+	
+	
+	
+	}
+
+	public static int doorToReveal1(int c, int u) //returns door number to reveal by utilizing logical operators
+	{
+			int r = (int) (Math.random() * 3 + 1);
+			while ( r == c || r == u)
+			{
+					r = (int) (Math.random() * 3 + 1);
+				
+				
+			}
+			return r;
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
